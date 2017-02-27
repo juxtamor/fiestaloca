3c5189fbdfff6c802b211a2ceb5cb8c48fead031
developpementjulio <julio.cruz@outlook.fr>
2017-02-27 10:42:27 +0100

    article=produit

--- apps/article.php
+++ apps/article.php
@@ -1,25 +1,25 @@
 <?php
 if(isset($_GET['id']))
 {
-	$manager = new ArticleManager($db);
-	$article = $manager->findById($_GET['id']);
+	$manager = new ProductManager($db);
+	$product = $manager->findById($_GET['id']);
 	// $id = intval($_GET['id']);
 	// $res = mysqli_query($db, "SELECT articles.*,users.login FROM  articles , users WHERE users.id=articles.id_author AND articles.id=".$id);
 	// $article = mysqli_fetch_assoc($res);
-	if($article)
+	if($product)
 	{
 		
-		require('views/article.phtml');
+		require('views/product.phtml');
 	}
 	else
 	{
-		$errors[]="L'article n'existe pas";
+		$errors[]="Le produit n'existe pas";
 		require('apps/errors.php');
 	}
 }
 else
 {
-	$errors[]="L'article n'existe pas";
+	$errors[]="Le produit n'existe pas";
 	require('apps/errors.php');
 }
 ?>
