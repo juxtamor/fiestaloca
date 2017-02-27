358e3f5b719968a4efb23c2eb00ab1c0f9493202
DJeWG <jeremy.weicheldinger@orange.fr>
2017-02-27 11:36:46 +0100

    MAJ Comment.Class et CommentManager.Class

--- models/CommentManager.class.php
+++ models/CommentManager.class.php
@@ -7,29 +7,19 @@ class CommentManager
 	{
 		$this->db = $db;
 	}
-	/*
-	public function findAll()
+
+	public function findByProd(Product $prod)
 	{
+		$id_prod = intval($prod->getId());
 		$list = [];
-		$res = mysqli_query($this->db, "SELECT * FROM comments ORDER BY date");
-		while ($comment = mysqli_fetch_object($res, "Comment"))
-		{
-			$list[] = $comment;
-		}
-		return $list;
-	}
-	*/
-	public function findByArticle(Article $article)
-	{
-		$id_article = intval($article->getId());
-		$list = [];
-		$res = mysqli_query($this->db, "SELECT * FROM comments WHERE id_article='".$id_article."' ORDER BY date DESC");
+		$res = mysqli_query($this->db, "SELECT * FROM comments WHERE id_prod='".$id_prod."' ORDER BY date DESC");
 		while ($comment = mysqli_fetch_object($res, "Comment",[$this->db]))
 		{
 			$list[] = $comment;
 		}
 		return $list;
 	}
+
 	public function findById($id)
 	{
 		$id = intval($id);
@@ -37,26 +27,29 @@ class CommentManager
 		$comment = mysqli_fetch_object($res, "Comment",[$this->db]);
 		return $comment;
 	}
+
 	public function save(Comment $comment)
 	{
 		$id = intval($comment->getId());
 		$content = mysqli_real_escape_string($this->db, $comment->getContent());
 		$id_author = intval($comment->getAuthor()->getId());
-		$id_article = intval($comment->getArticle()->getId());
-		$res = mysqli_query($this->db, "UPDATE comments SET content='".$content."', id_author='".$id_author."', id_article='".$id_article."' WHERE id='".$id."' LIMIT 1");
+		$id_article = intval($comment->getProd()->getId());
+		$res = mysqli_query($this->db, "UPDATE comments SET content='".$content."', id_author='".$id_author."', id_prod='".$id_prod."' WHERE id='".$id."' LIMIT 1");
 		if (!$res)
 		{
 			throw new Exceptions(["Erreur interne"]);
 		}
 		return $this->findById($id);
 	}
+
 	public function remove(Comment $comment)
 	{
 		$id = intval($comment->getId());
 		mysqli_query($this->db, "DELETE from comments WHERE id='".$id."' LIMIT 1");
 		return $comment;
 	}
-	public function create($content, User $author, Article $article)
+
+	public function create($content, User $author, Product $prod)
 	{
 		$errors = [];
 		$comment = new Comment($this->db);
@@ -70,7 +63,7 @@ class CommentManager
 		{
 			$errors[] = $error;
 		}
-		$error = $comment->setArticle($article);
+		$error = $comment->setProd($prod);
 		if ($error)
 		{
 			$errors[] = $error;
@@ -81,8 +74,8 @@ class CommentManager
 		}
 		$content = mysqli_real_escape_string($this->db, $comment->getContent());
 		$id_author = intval($comment->getAuthor()->getId());
-		$id_article = intval($comment->getArticle()->getId());
-		$res = mysqli_query($this->db, "INSERT INTO comments (content, id_author, id_article) VALUES('".$content."', '".$id_author."', '".$id_article."')");
+		$id_prod = intval($comment->getProd()->getId());
+		$res = mysqli_query($this->db, "INSERT INTO comments (content, id_author, id_prod) VALUES('".$content."', '".$id_author."', '".$id_prod."')");
 		if (!$res)
 		{
 			throw new Exceptions(["Erreur interne"]);
