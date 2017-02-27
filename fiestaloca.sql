f4502ff4a2ec78ce42c2a752dd265f36b115abe0
juxtamor <julien.hzt@gmail.com>
2017-02-27 09:33:20 +0100

    sql

--- fiestaloca.sql
+++ fiestaloca.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Lun 27 Février 2017 à 09:15
+-- Généré le: Lun 27 Février 2017 à 09:32
 -- Version du serveur: 5.5.53-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.20
 
@@ -28,8 +28,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `categorie` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `cat_name` varchar(31) NOT NULL,
-  `cat_desc` varchar(4095) NOT NULL,
+  `cat_name` varchar(31) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `cat_desc` varchar(4095) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -41,10 +41,11 @@ CREATE TABLE IF NOT EXISTS `categorie` (
 
 CREATE TABLE IF NOT EXISTS `command` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `status` varchar(127) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `status` varchar(127) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `id_user` int(10) unsigned NOT NULL,
   `total_price` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id_user` (`id_user`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -55,12 +56,14 @@ CREATE TABLE IF NOT EXISTS `command` (
 
 CREATE TABLE IF NOT EXISTS `comments` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `content` varchar(4095) NOT NULL,
-  `id_author` int(11) NOT NULL,
-  `id_prod` int(11) NOT NULL,
+  `content` varchar(4095) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `id_author` int(10) unsigned NOT NULL,
+  `id_prod` int(10) unsigned NOT NULL,
   `note` int(11) NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `id_author` (`id_author`),
+  KEY `id_prod` (`id_prod`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -70,8 +73,10 @@ CREATE TABLE IF NOT EXISTS `comments` (
 --
 
 CREATE TABLE IF NOT EXISTS `link_command_products` (
-  `id_command` int(11) NOT NULL,
-  `id_products` int(11) NOT NULL
+  `id_command` int(10) unsigned NOT NULL,
+  `id_products` int(10) unsigned NOT NULL,
+  KEY `id_command` (`id_command`),
+  KEY `id_products` (`id_products`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -82,14 +87,15 @@ CREATE TABLE IF NOT EXISTS `link_command_products` (
 
 CREATE TABLE IF NOT EXISTS `products` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `prod_name` varchar(31) NOT NULL,
-  `prod_desc` varchar(4095) NOT NULL,
+  `prod_name` varchar(31) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `prod_desc` varchar(4095) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `price` int(11) NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `stock` int(11) NOT NULL,
-  `id_category` int(11) NOT NULL,
-  `prod_cover` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+  `id_category` int(10) unsigned NOT NULL,
+  `prod_cover` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_category` (`id_category`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
@@ -100,14 +106,46 @@ CREATE TABLE IF NOT EXISTS `products` (
 
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `login` varchar(31) NOT NULL,
-  `password` varchar(63) NOT NULL,
-  `email` varchar(63) NOT NULL,
-  `adress` varchar(511) NOT NULL,
+  `login` varchar(31) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(63) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `email` varchar(63) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `adress` varchar(511) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   `birthdate` date NOT NULL,
   `admin` tinyint(4) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `login` (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `command`
+--
+ALTER TABLE `command`
+  ADD CONSTRAINT `command_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_author`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`id_prod`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `link_command_products`
+--
+ALTER TABLE `link_command_products`
+  ADD CONSTRAINT `link_command_products_ibfk_2` FOREIGN KEY (`id_products`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `link_command_products_ibfk_1` FOREIGN KEY (`id_command`) REFERENCES `command` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Contraintes pour la table `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`id_category`) REFERENCES `categorie` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
