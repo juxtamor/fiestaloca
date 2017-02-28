e07d63c34c57ec239361018e78039e80eada8383
juxtamor <julien.hzt@gmail.com>
2017-02-28 10:10:49 +0100

    màj index

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $errors=[];
 // }
 $db = mysqli_connect("192.168.1.79","fiestaloca","fiestaloca","fiestaloca"); //URL, Utilisateur, MdP, Base de données//
 session_start();// http://php.net/manual/fr/function.session-start.php
-$access = ["articles", "login", "register", "create_article", "edit_article", "article", "accueil"];
+$access = ["products", "login", "register", "create_product", "edit_product", "product", "accueil"];
 $page = "accueil";
 if (isset($_GET['page']) && in_array($_GET['page'], $access))
 {
