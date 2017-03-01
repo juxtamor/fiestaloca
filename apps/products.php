c30b150407f4baad932e95b3517154f376131398
juxtamor <julien.hzt@gmail.com>
2017-03-01 10:52:17 +0100

    products : affichage produit par cat

--- apps/products.php
+++ apps/products.php
@@ -1,5 +1,23 @@
 <?php
-$manager = new ProductManager($db);
-$list = $manager->findAll();
-require('views/products.phtml');
+/*while ($article = mysqli_fetch_assoc($res))
+{
+        require('views/articles_elem.phtml');
+}*/
+if (isset($_GET['id_category']))
+{
+        $categoryManager = new CategoryManager($db);
+        $category = $categoryManager->findById($_GET['id_category']);
+        $manager = new ProductManager($db);
+        $list = $manager->findByCategory($category);
+
+        $count = 0;
+        while ($count < count($list))// list.length
+        {
+                $product = $list[$count];
+                require('views/products.phtml');
+                $count++;
+        }
+}
+else
+        echo "taggle";
 ?>
