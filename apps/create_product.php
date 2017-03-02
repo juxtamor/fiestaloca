9a183597a1c2947894dbb67a07addc124dc9985d
emeline1 <emeline.zovic@gmail.com>
2017-03-02 10:35:58 +0100

    apps

--- apps/create_product.php
+++ apps/create_product.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SESSION['admin'] = 1)
+if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1)
 {
 require('views/create_product.phtml');
 }
--- apps/del_comment.php
+++ apps/del_comment.php
@@ -1,3 +1,6 @@
 <?php
+if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1)
+{
 require('views/del_comment.phtml');
+}
 ?>
