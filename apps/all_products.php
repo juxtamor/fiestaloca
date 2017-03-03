476f21963f10bb515ca79251d79d62561eaae34a
emeline1 <emeline.zovic@gmail.com>
2017-03-03 10:14:19 +0100

    all

---
+++ apps/all_products.php
@@ -0,0 +1,5 @@
+<?php
+
+require('views/all_products.phtml');
+
+?>
--- apps/products.php
+++ apps/products.php
@@ -4,12 +4,13 @@ if (isset($_GET['id_category']))
         $categorieManager = new CategorieManager($db);
         $category = $categorieManager->findById($_GET['id_category']);
         $list = $category->getProducts();
+        
 		require('views/products.phtml');        
 }
 else
 {
 	$manager = new ProductManager($db);
 	$list = $manager->findAll();
-	require('views/products.phtml');
+	require('views/all_products.phtml');
 }
 ?>
